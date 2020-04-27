4339f814118055a60231bcca289bb642ab8d8bf0
Santa claus <santa@northpole.com>
2020-04-28 01:01:16 +0300

    addition of classes to models.py

---
+++ news-list/app/models.py
@@ -0,0 +1,29 @@
+class Source:
+    '''
+    Source class to define Source Objects
+    '''
+    def __init__(self,id,name,description,url,category,language,country):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.language = language
+        self.country = country
+
+
+class News:
+    '''
+    Categorized news class to define News Objects
+    '''
+
+    def __init__(self,id,name, author,title, description, url, urlToImage, publishedAt, content):
+        self.id = id
+        self.name = name
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
